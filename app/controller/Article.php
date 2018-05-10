0b24891d067bc33365cbc9cf0b402ad8c23c6009
Dongasai <1514582970@qq.com>
2018-05-10 16:21:34 +0800

    一些

---
+++ app/controller/Article.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace app\controller;
+
+use app\Controller;
+
+/**
+ * 文章控制器
+ * Class Article
+ * @package app\controller
+ */
+class Article extends Controller
+{
+
+    public function index()
+    {
+
+    }
+
+    public function add()
+    {
+
+    }
+
+
+    public function edit()
+    {
+
+    }
+
+    public function dele()
+    {
+
+    }
+
+    public function info()
+    {
+
+    }
+}
